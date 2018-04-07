09f1fa36df66f6aa55773b94b703e8f3b3a6b8a6
DELL <gor_nick987@mail.ru>
2018-04-08 02:58:35 +0300

    add db

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@ const config = require('config');
 const {commit, repo} = require('./models');
 
 module.exports = (Sequelize) => {
-	const sequelize = new Sequelize(config.db.name, config.db.user, config.db.pass, config.db.options);
+
+	const sequelize = new Sequelize(config.db.name,
+			config.db.user, config.db.pass, config.db.options);
 
 	const Commit = commit(Sequelize, sequelize);
 	const Repo = repo(Sequelize, sequelize);
